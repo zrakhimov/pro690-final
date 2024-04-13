e9caa96ad32839f3e2b56c9363b7a5303a2f04e9
zokir.rakhimov@gmail.com <zokir.rakhimov@gmail.com>
2024-04-13 18:09:30 -0400

    initial

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnet_web_app.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
